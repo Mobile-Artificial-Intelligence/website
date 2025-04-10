d00d188a3012661c37dd2367fb8032cfd1b7cd44
danemadsen <dane_madsen@hotmail.com>
2025-04-10 10:48:48 +1000

    logo

--- src/Topbar.tsx
+++ src/Topbar.tsx
@@ -5,7 +5,10 @@ function Topbar() {
         <header>
             <div className='logoGroup'>
                 <img src="/logo.svg" alt="logo" />
-                <h1>Mobile Artificial Intelligence</h1>
+                <h1>
+                  <span className="full">Mobile Artificial Intelligence</span>
+                  <span className="short">Mobile AI</span>
+                </h1>
             </div>
             <div className='linkGroup'>
                 <a href="https://github.com/orgs/Mobile-Artificial-Intelligence/repositories">Github</a>
