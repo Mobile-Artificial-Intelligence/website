28d56861fbf2588a27dc1818af49ac2f949a3fb1
danemadsen <dane_madsen@hotmail.com>
2025-04-11 14:55:40 +1000

    mobile

--- src/components/MaidCarousel.tsx
+++ src/components/MaidCarousel.tsx
@@ -7,48 +7,71 @@ function MaidCarousel() {
     useEffect(() => {
         const carousel = carouselRef.current;
         if (!carousel) return;
-
+    
         let isDown = false;
         let startX = 0;
         let scrollLeft = 0;
-
-        const mouseDown = (e: MouseEvent) => {
+    
+        const startDragging = (x: number) => {
             isDown = true;
             carousel.classList.add('dragging');
-            startX = e.pageX - carousel.offsetLeft;
+            startX = x - carousel.offsetLeft;
             scrollLeft = carousel.scrollLeft;
         };
-
-        const mouseLeave = () => {
+    
+        const stopDragging = () => {
             isDown = false;
             carousel.classList.remove('dragging');
         };
-
-        const mouseUp = () => {
-            isDown = false;
-            carousel.classList.remove('dragging');
+    
+        const doDragging = (x: number) => {
+            if (!isDown) return;
+            const walk = x - carousel.offsetLeft - startX;
+            carousel.scrollLeft = scrollLeft - walk;
         };
-
+    
+        // Mouse events
+        const mouseDown = (e: MouseEvent) => startDragging(e.pageX);
         const mouseMove = (e: MouseEvent) => {
             if (!isDown) return;
             e.preventDefault();
-            const x = e.pageX - carousel.offsetLeft;
-            const walk = x - startX;
-            carousel.scrollLeft = scrollLeft - walk;
+            doDragging(e.pageX);
         };
-
+        const mouseUp = stopDragging;
+        const mouseLeave = stopDragging;
+    
+        // Touch events
+        const touchStart = (e: TouchEvent) => {
+            if (e.touches.length === 1) {
+                startDragging(e.touches[0].pageX);
+            }
+        };
+        const touchMove = (e: TouchEvent) => {
+            if (!isDown) return;
+            doDragging(e.touches[0].pageX);
+        };
+        const touchEnd = stopDragging;
+    
         carousel.addEventListener('mousedown', mouseDown);
-        carousel.addEventListener('mouseleave', mouseLeave);
-        carousel.addEventListener('mouseup', mouseUp);
         carousel.addEventListener('mousemove', mouseMove);
-
+        carousel.addEventListener('mouseup', mouseUp);
+        carousel.addEventListener('mouseleave', mouseLeave);
+    
+        carousel.addEventListener('touchstart', touchStart, { passive: false });
+        carousel.addEventListener('touchmove', touchMove, { passive: false });
+        carousel.addEventListener('touchend', touchEnd);
+    
         return () => {
             carousel.removeEventListener('mousedown', mouseDown);
-            carousel.removeEventListener('mouseleave', mouseLeave);
-            carousel.removeEventListener('mouseup', mouseUp);
             carousel.removeEventListener('mousemove', mouseMove);
+            carousel.removeEventListener('mouseup', mouseUp);
+            carousel.removeEventListener('mouseleave', mouseLeave);
+    
+            carousel.removeEventListener('touchstart', touchStart);
+            carousel.removeEventListener('touchmove', touchMove);
+            carousel.removeEventListener('touchend', touchEnd);
         };
-    }, []);
+    }, []);    
 
     return (
         <div id="maid-carousel" ref={carouselRef}>
